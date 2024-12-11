e6f262f0814b123d48792b72126a20f52c0e2495
Gabriel Valdez <valdezga@oregonstate.edu>
2024-12-10 20:33:35 -0800

    Update deleteMember.php

--- deleteMember.php
+++ deleteMember.php
@@ -16,8 +16,9 @@
 
             if($stmt = mysqli_prepare($link, $sql)){
                 // Bind variables to the prepared statement as paramters
-                mysqli_stmt_bind_param($stmt, "s", $member_id);
-
+                mysqli_stmt_bind_param($stmt, "s", $param_memid);
+        
+                $param_memid = $member_id;
                 // Attempt to execute the prepared statement
                 if(mysqli_stmt_execute($stmt)){
                     header("location: index.php");
